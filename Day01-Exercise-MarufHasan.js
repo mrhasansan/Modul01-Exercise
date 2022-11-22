6f93fe63c3d8184ed158ec83a95704d8eff936b2
marufhasan <marufhasansan@gmail.com>
2022-11-22 12:35:46 +0700

    update return function dan export test driven)

--- Day01-Exercise-MarufHasan.js
+++ Day01-Exercise-MarufHasan.js
@@ -2,7 +2,7 @@
 // Write a code to find area of rectangle.
 function luasPersegi(sisi) {
   var areaPersegi = Math.pow(sisi, 2);
-  console.log(areaPersegi);
+  return areaPersegi;
 }
 luasPersegi(8);
 
@@ -10,7 +10,7 @@ luasPersegi(8);
 // Write a code to find perimeter of rectangle.
 function kelPer(sisi) {
   var kelilingPersegi = 4 * sisi;
-  console.log(kelilingPersegi);
+  return kelilingPersegi;
 }
 kelPer(100);
 
@@ -18,15 +18,15 @@ kelPer(100);
 //Write a code to find diameter, circumference and area of a circle.
 function diameter(jari) {
   var diameter = jari * 2;
-  console.log(diameter);
+  return diameter;
 }
 function kelilingLingkaran(jari) {
   var kelilingLingkaran = 2 * (22 / 7) * jari;
-  console.log(kelilingLingkaran);
+  return kelilingLingkaran;
 }
 function luaslingkaran(jari) {
   var luasLingkaran = (22 / 7) * Math.pow(jari, 2);
-  console.log(luasLingkaran);
+  return luasLingkaran;
 }
 
 diameter(7);
@@ -37,7 +37,7 @@ luaslingkaran(7);
 // Write a code to find angles of triangle if two angles are given
 function sudut(sudutA, sudutB) {
   var sudutC = 180 - (sudutA + sudutB);
-  console.log(sudutC);
+  return sudutC;
 }
 sudut(34, 90);
 
@@ -46,20 +46,31 @@ sudut(34, 90);
 function bedahari(date1, date2) {
   const beda = date2.getTime() - date1.getTime();
   const totalbedaHari = Math.ceil(beda / (24 * 60 * 60 * 1000));
-  console.log(totalbedaHari);
+  return totalbedaHari;
 }
 const date1 = new Date("2015-08-10");
 const date2 = new Date("2015-08-12");
 bedahari(date1, date2);
 
 //no 6
-function hari(totalhari) {
+function hariConversi(totalhari) {
   var tahun = Math.floor(totalhari / 365);
   var sisahari = totalhari % 365;
   var bulan = Math.floor(sisahari / 30);
   var hari = sisahari % 30;
   var hasil = `${totalhari} hari :  ${tahun} tahun, ${bulan} bulan, ${hari} hari`;
-  console.log(hasil);
+  return hasil;
 }
 
-hari(900);
+hariConversi(900);
+
+module.exports = {
+  luasPersegi,
+  kelPer,
+  diameter,
+  kelilingLingkaran,
+  luaslingkaran,
+  sudut,
+  bedahari,
+  hariConversi,
+};
